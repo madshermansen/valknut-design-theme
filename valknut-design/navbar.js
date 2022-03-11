ea97e6891d05e6f902e8f159c3d12927f7a0e338
Mads Hermansen <mads.d.hermansen@gmail.com>
2022-03-11 17:48:16 +0100

    nav

--- valknut-design/navbar.js
+++ valknut-design/navbar.js
@@ -14,6 +14,12 @@ var runhamburgernav = function() {
     mobilenav.style.zIndex = 101
     mobilenav.style.opacity = 1
     mobilenav.style.visibility = "visible"
+    closingnav.style.zIndex = 101
+    setTimeout(() => {
+      navmobile.style.zIndex = 100
+      mobilenav.style.zIndex = 101
+      closingnav.style.zIndex = 101
+ }, 500);
 
   } else {
     hamburgernav.classList.remove('open');
@@ -23,9 +29,14 @@ var runhamburgernav = function() {
   }
   mobilenav.style.opacity = 0
   mobilenav.style.visibility = "hidden"
-  mobilenav.style.zIndex = 0
-  navmobile.style.zIndex = 0
-  closingnav.style.zIndex = -1
+  setTimeout(() => {
+       mobilenav.style.zIndex = 0
+       navmobile.style.zIndex = 0
+       closingnav.style.zIndex = -1
+  }, 500);
+  // mobilenav.style.zIndex = 0
+  // navmobile.style.zIndex = 0
+  // closingnav.style.zIndex = -1
     closingnav.innerHTML = ""
     closingnav.innerHTML = '<object class="nav-mobile close-nav" id="hidden-nav" type="image/svg+xml" data="wp-content/themes/valknut-design/assets/svg-animations/mobile-nav-reverse.svg"></object><object class="nav-mobile hidden-nav" id="hidden-nav" type="image/svg+xml" data="wp-content/themes/valknut-design/assets/svg-animations/mobile-nav-reverse.svg"></object><object class="nav-mobile close-nav" id="hidden-nav" type="image/svg+xml" data="wp-content/themes/valknut-design/assets/svg-animations/mobile-nav-reverse.svg"></object><object class="nav-mobile hidden-nav" id="hidden-nav" type="image/svg+xml" data="../wp-content/themes/valknut-design/assets/svg-animations/mobile-nav-reverse.svg"></object>'
   }
